2f437a26bc2ef794ffda8453be62d625b658ec52
jibnorge <jibingeorge7337@gmail.com>
2023-12-06 20:05:05 -0500

    Update database.py

--- database.py
+++ database.py
@@ -2,8 +2,12 @@ import pandas as pd
 from sqlalchemy import create_engine
 import sqlite3
 
-df = pd.read_csv('books_updated.csv')
+df_books = pd.read_csv('books_updated.csv')
+
+df_features = pd.read_csv('features.csv')
 
 engine = create_engine('sqlite:///books.db')
 
-df.to_sql('table_name', engine, index = False, if_exists = 'replace')
+df_books.to_sql('book_table', engine, index = False, if_exists = 'replace')
+
+df_features.to_sql('feature_table', engine, index = False, if_exists = 'replace')
